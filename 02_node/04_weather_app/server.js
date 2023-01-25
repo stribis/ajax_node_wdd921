b423b5d6392afbb844c610edf775c150bb3bb42e
Martin Hutchings <martin.hutchings@sae.edu>
2023-01-25 19:10:42 +0100

    add nodejs weatherApp

--- 02_node/04_weather_app/server.js
+++ 02_node/04_weather_app/server.js
@@ -17,13 +17,16 @@ const apiKey = 'af1eb7ff29c464bd26db09fc2c06ca09'
 
 app.get('/', (req, res) => {
   // Render index page
-  res.render('index.ejs')
+  res.render('index.ejs', {
+    data: null,
+    error: null
+  })
 })
 
 app.post('/', (req, res) => {
   const city = req.body.city
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=en`
-  getData(url)
+  const url = `https://api.openweatherma.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=en`
+  getData(url, res)
   
 })
 
@@ -33,9 +36,36 @@ app.listen(3000, () => {
 })
 
 // Fetch Request for Openweathermap API
-async function getData (url) {
-  const response = await fetch(url)
-  const data = await response.json()
+async function getData (url, res) {
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+  
 
-  console.log(data)
+    if ( data.cod === '404') {
+      res.render('index.ejs', {
+        data: null,
+        error: 'The city was not found in the API'
+      })
+    } else {
+      const datapack = {
+        temp: data.main.temp,
+        description: data.weather[0].description,
+        icon: data.weather[0].icon,
+        feels: data.main.feels_like
+      }
+    
+      res.render('index.ejs', {
+        data: datapack,
+        error: null
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    res.render('index.ejs', {
+      data: null,
+      error: 'Connection error with the api'
+    })
+  }
+
 }
